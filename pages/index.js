1eefabb2908be9b2e1db844fb1baa5de091c08f0
samclement <sam@swhurl.com>
2017-12-05 23:40:20 +0000

    Update homepage

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Title = styled.h1`
-  color: red;
-  font-size: 50px;
+  color: deeppink;
 `
 
-export default () => <Title>My page</Title>
+export default () => {
+  return (
+    <div>
+      <Title>swhurl.com</Title>
+      <p>Example next.js app, including:</p>
+      <ul>
+        <li>styled components</li>
+      </ul>
+    </div>
+  )
+}
