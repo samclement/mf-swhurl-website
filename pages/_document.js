0973d422a30a7ffc18e78c0bbb80f747e149da1b
samclement <sam@swhurl.com>
2017-12-12 00:17:58 +0000

    Add swhurl.com to document title

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>My page</title>
+          <title>swhurl.com</title>
           {this.props.styleTags}
         </Head>
         <body>
