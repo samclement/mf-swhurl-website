6b8e056259d07ab8aa71435e7ab790648ef105d9
Sam Clement <sam@swhurl.com>
2018-01-07 10:14:44 +0000

    chore: add temp favicon

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>swhurl.com</title>
+          <link rel="icon" type="images/x-icon" href="/static/favicon.ico" />
           {this.props.styleTags}
         </Head>
         <body>
