2ddde0b53c8b74a459bbd64f0b4fa088af81183d
Sam Clement <sam@swhurl.com>
2018-01-08 23:01:58 +0000

    test: initial e2e test

---
+++ test/spec/homepage.spec.js
@@ -0,0 +1,10 @@
+describe('homepage', () => {
+  it('title', () => {
+    browser.url('/');
+    browser.getTitle().should.be.equal('swhurl.com');
+  })
+  it('header', () => {
+    browser.url('/')
+    browser.getText('h1').should.be.equal('swhurl.com')
+  })
+})
