a4a4cfc3bd3ddcc3fa4d4ba46f710739ebc3de69
Sam Clement <sam@swhurl.com>
2018-01-10 17:46:51 +0000

    test: added navigation tests

---
+++ test/spec/navigation.spec.js
@@ -0,0 +1,14 @@
+describe('main navigation', () => {
+  it('should navigate to the About page', () => {
+    navigate('About')
+  })
+  it('should navigate to the Contact page', () => {
+    navigate('Contact')
+  })
+})
+
+function navigate(header) {
+  browser.url('/')
+  browser.element(`a*=${header}`).click()
+  browser.getText('h1').should.be.equal(header)
+}
