a58364f4d2bd94fec770b3138532c76f844d66a6
Sam Clement <sam@swhurl.com>
2018-02-07 17:18:02 +0000

    chore: add production sourcemaps

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-const path = require('path');
-const glob = require('glob');
-
 module.exports = {
   webpack: (config, { dev }) => {
     config.module.rules.push({
       test: /\.test.js$/,
       loader: 'ignore-loader'
     })
+    if(!dev) {
+      config.devtool = 'source-map'
+      for (const options of config.plugins) {
+        if (options['constructor']['name'] === 'UglifyJsPlugin') {
+          options.options.sourceMap = true;
+          break;
+        }
+      }
+    }
     return config;
   }
 }
