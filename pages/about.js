41f1da62dc2411e7223a7ee9b654538c92f580fd
Sam Clement <sam@swhurl.com>
2018-04-21 08:23:34 +0100

    refactor: move react components into a components directory

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import H1 from '../elements/H1.js'
-import Nav from '../blocks/Nav'
+import H1 from '../components/elements/H1.js'
+import Nav from '../components/blocks/Nav'
 
 export default () => {
   return (
