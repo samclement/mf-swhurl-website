2822b086a9b4886fc46c6747cff463a5d84d0e67
Sam Clement <sam@swhurl.com>
2018-10-10 17:08:27 +0100

    refactor: change to public registry

--- publish.sh
+++ publish.sh
@@ -4,21 +4,21 @@ if [ $# -eq 0 ]
     echo "Please supply target environemnt as the only argument (staging|production)"
 fi
 
-echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin registry.swhurl.com
+echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
 while test $# -gt 0; do
   case "$1" in
     staging)
       shift
       echo "Publishing staging image"
-      docker build --build-arg DRONE_COMMIT=${DRONE_COMMIT} -t registry.swhurl.com/swhurl/website:staging .
-      docker push registry.swhurl.com/swhurl/website:staging
+      docker build --build-arg DRONE_COMMIT=${DRONE_COMMIT} -t swhurl/website:staging .
+      docker push swhurl/website:staging
       ;;
     production)
       shift
       GIT_TAG=${DRONE_TAG##v}
       echo "Publishing production image: $GIT_TAG"
-      docker build --build-arg DRONE_COMMIT=${DRONE_COMMIT} --build-arg DRONE_TAG=$GIT_TAG -t registry.swhurl.com/swhurl/website:$GIT_TAG .
-      docker push registry.swhurl.com/swhurl/website:$GIT_TAG
+      docker build --build-arg DRONE_COMMIT=${DRONE_COMMIT} --build-arg DRONE_TAG=$GIT_TAG -t swhurl/website:$GIT_TAG .
+      docker push swhurl/website:$GIT_TAG
       ;;
     *)
       echo "Please supply either 'staging' or 'production' as the target environment"
