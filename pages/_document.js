feda5936bb725a0654cffb5a4ab2e4d809efbd72
Sam Clement <sam@swhurl.com>
2018-12-14 20:16:58 +0000

    fix: ssr styles

--- pages/_document.js
+++ pages/_document.js
@@ -11,15 +11,11 @@ export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-      })
+    ctx.renderPage = () => originalRenderPage({
+      enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+    })
     const initialProps = await Document.getInitialProps(ctx)
-    return {
-      ...initialProps,
-      styles: [...initialProps.styles, ...sheet.getStyleElement()]
-    }
+    return { ...initialProps, styles: [...initialProps.styles, ...sheet.getStyleElement()] }
   }
   render() {
     return (
