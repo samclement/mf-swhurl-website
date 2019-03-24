8ba292da89074666f4f705c8da1b265c5414924b
Sam Clement <sam@swhurl.com>
2019-03-24 08:03:40 +0000

    feat: add navigation and catalue api endpoints

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const express = require('express')
 const next = require('next')
+const request = require('request')
+const parser = require('./lib/plp.js')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -19,9 +21,54 @@ app.prepare().then(() => {
       .pipe(res)
   })
 
+  // API
+
   server.get('/api/demo', (req, res) => {
     res.send({ message: 'Remote message.' })
   })
+
+  // Navigation
+
+  server.get('/api/navigation/:gender', (req, res) => {
+    const opts = {
+      url: `https://www.matchesfashion.com/${req.params.gender}/shop/clothing?format=json`,
+      headers: {
+        'User-Agent': 'API'
+      }
+    }
+    request(opts, (err, response, body) => {
+      if (err) {
+        console.log(err)
+      }
+      const parsed = JSON.parse(body)
+      const data = parser.parseNavigation(parsed.level1CategoriesData)
+      res.send(data)
+    })
+  })
+
+  // Catalogue
+
+  server.get('/api/catalogue/*', (req, res) => {
+    const cleanPath = req.url.replace(/^\/api\/catalogue\//gi, '')
+    const delimiter = cleanPath.includes('?') ? '&' : '?'
+    const formatPath = `${cleanPath}${delimiter}format=json`
+    const opts = {
+      url: `https://www.matchesfashion.com/${formatPath}`,
+      headers: {
+        'User-Agent': 'API'
+      }
+    }
+    request(opts, (err, response, body) => {
+      if (err) {
+        console.log(err)
+      }
+      const parsed = JSON.parse(body)
+      const data = parser.parseSearchResults(parsed)
+      res.send(data)
+    })
+  })
+
+  // PAGES
 
   server.get('*', (req, res) => {
     handle(req, res)
