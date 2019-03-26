6512264f9400fa372f7c11ff85244f64414573bb
Sam Clement <sam@swhurl.com>
2019-03-26 21:21:27 +0000

    fix: align checkboxes to filter labels

--- components/blocks/FilterBlock/index.js
+++ components/blocks/FilterBlock/index.js
@@ -22,6 +22,21 @@ const StyledLink = styled.a`
   color: black;
   font-size: 11px;
   text-decoration: none;
+  display: block;
+  padding-left: 15px;
+  text-indent: -15px;
+  margin-bottom: 5px;
+`
+
+const StyledInput = styled.input`
+  width: 13px;
+  height: 13px;
+  padding: 0;
+  margin: 0;
+  vertical-align: bottom;
+  position: relative;
+  top: -1px;
+  *overflow: hidden;
 `
 
 export default ({ title, filters }) => {
@@ -34,7 +49,7 @@ export default ({ title, filters }) => {
           return (
             <StyledLi key={i}>
               <Link prefetch key={i} href={url} as={f.url}>
-                <StyledLink href="#"><input type="checkbox" readOnly checked={f.selected} /> {f.name}</StyledLink>
+                <StyledLink href="#"><StyledInput type="checkbox" readOnly checked={f.selected} /> {f.name}</StyledLink>
               </Link>
             </StyledLi>
           )
