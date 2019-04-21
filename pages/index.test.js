d5d1fc0f70fe11e4ebf15767154b3bdccd7aa28c
Sam Clement <sam@swhurl.com>
2019-04-21 11:20:44 +0100

    refactor: test index page using react-testing-library

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,14 +1,18 @@
 /* eslint-env jest */
 
 import React from 'react'
-import { render } from 'enzyme'
+import { render } from 'react-testing-library'
 import 'jest-styled-components'
-
+import 'jest-dom/extend-expect'
 import Index from './index.js'
+// Nextjs routing support
+import Router from 'next/router'
+const mockedRouter = { push: () => {}, prefetch: () => {} }
+Router.router = mockedRouter
 
 describe('With Snapshot Testing', () => {
   it('Index page shows "mf.swhurl.com" H1', () => {
-    const wrapper = render(<Index />)
-    expect(wrapper).toMatchSnapshot()
+    const { asFragment } = render(<Index />)
+    expect(asFragment()).toMatchSnapshot()
   })
 })
