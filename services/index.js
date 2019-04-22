9752ce7128f022b2c70b3f4390debcd1fe52d97f
Sam Clement <sam@swhurl.com>
2019-04-22 10:38:11 +0100

    fix: preserve scroll position when using the back button by caching api results

--- services/index.js
+++ services/index.js
@@ -1,4 +1,13 @@
 import axios from 'axios'
+import { setupCache } from 'axios-cache-adapter'
+
+const cache = setupCache({
+  maxAge: 5 * 60 * 1000
+})
+
+const api = axios.create({
+  adapter: cache.adapter
+})
 
 export const getApiUrl = (isServer = true) => {
   return isServer ? 'http://localhost:3000' : `//${window.location.hostname}:${window.location.port}`
@@ -6,13 +15,13 @@ export const getApiUrl = (isServer = true) => {
 
 export const getNav = async function (req) {
   const isServer = !!req
-  return axios(`${getApiUrl(isServer)}/api/navigation/mens`)
+  return api(`${getApiUrl(isServer)}/api/navigation/mens`)
 }
 
 export const getSearchResults = async function (q, isServer) {
-  return axios(`${getApiUrl(isServer)}/api/catalogue${q.url}`)
+  return api(`${getApiUrl(isServer)}/api/catalogue${q.url}`)
 }
 
 export const getProduct = async function (q, isServer) {
-  return axios(`${getApiUrl(isServer)}/api/products/${q.code}`)
+  return api(`${getApiUrl(isServer)}/api/products/${q.code}`)
 }
