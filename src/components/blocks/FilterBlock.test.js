402ab00547665d3fac335c65814cbffc6cb708bf
Sam Clement <sam@swhurl.com>
2019-09-07 13:43:56 +0100

    fix: update jest-dom to @testing-library/jest-dom

--- src/components/blocks/FilterBlock.test.js
+++ src/components/blocks/FilterBlock.test.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import 'jest-styled-components'
-import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
 import FilterBlock from './FilterBlock.js'
 // Data
 import data from '../../../test-data/activewear.json'
