6e87155852339b6a10c1b354345acae6e263e7d3
Sam Clement <sam@swhurl.com>
2020-02-04 08:16:34 +0000

    Test reporting (#89)
    * test: add html reporter
    
    * test: remove test report from repo
    
    * chore: update test report dependency

--- send-test-output.sh
+++ send-test-output.sh
@@ -1,3 +1,5 @@
 cp -r /www/test/output /www/build-${CIRCLE_BUILD_NUM} && mc cp -r /www/build-${CIRCLE_BUILD_NUM} swhurl/mf-test-output/
+echo '\n***************\n'
 echo https://minio.swhurl.com/mf-test-output/build-${CIRCLE_BUILD_NUM}/jest/lcov-report/index.html
 echo https://minio.swhurl.com/mf-test-output/build-${CIRCLE_BUILD_NUM}/lighthouse/lighthouse.report.html
+echo '\n***************\n'
