fe00f9ba8d269c31fefa31110182b1417d9f198c
Sam Clement <sam@swhurl.com>
2020-02-07 18:55:52 +0000

    refactor: remove send-test-output.sh

--- send-test-output.sh
+++
@@ -1,5 +0,0 @@
-cp -r /www/test/output /www/build-${CIRCLE_BUILD_NUM} && mc cp -r /www/build-${CIRCLE_BUILD_NUM} swhurl/mf-test-output/
-mc policy set download swhurl/mf-test-output/build-${CIRCLE_BUILD_NUM}/index.html
-echo '\n***************\n'
-echo https://minio.swhurl.com/mf-test-output/build-${CIRCLE_BUILD_NUM}/index.html
-echo '\n***************\n'
